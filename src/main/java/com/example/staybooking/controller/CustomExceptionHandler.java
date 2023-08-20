28e69dbf0e74e58c6cde97c16ec54272ae792ee1
Long-Qin <mrmrmr.wayne@gmail.com>
2023-08-19 21:23:03 -0400

    added a stay upload image function to the Stay controller allowing MultipartFile submit from web

--- src/main/java/com/example/staybooking/controller/CustomExceptionHandler.java
+++ src/main/java/com/example/staybooking/controller/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.staybooking.controller;
 
+import com.example.staybooking.exception.GCSUploadException;
 import com.example.staybooking.exception.StayNotExistException;
 import com.example.staybooking.exception.UserAlreadyExistException;
 import com.example.staybooking.exception.UserNotExistException;
@@ -25,5 +26,10 @@ public class CustomExceptionHandler {
     @ExceptionHandler(StayNotExistException.class)
     public final ResponseEntity<String> handleStayNotExistException(Exception ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(GCSUploadException.class)
+    public final ResponseEntity<String> handleGCSUploadException(Exception ex, WebRequest request){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/example/staybooking/service/StayService.java
+++ src/main/java/com/example/staybooking/service/StayService.java
@@ -2,19 +2,27 @@ package com.example.staybooking.service;
 
 import com.example.staybooking.exception.StayNotExistException;
 import com.example.staybooking.model.Stay;
+import com.example.staybooking.model.StayImage;
 import com.example.staybooking.model.User;
 import com.example.staybooking.repository.StayRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.*;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StayService {
 
-    private StayRepository stayRepository;
+    private final StayRepository stayRepository;
+    private final ImageStorageService imageStorageService;
 
-    public StayService(StayRepository stayRepository) {
+    public StayService(StayRepository stayRepository, ImageStorageService imageStorageService) {
+
         this.stayRepository = stayRepository;
+        this.imageStorageService = imageStorageService;
     }
 
     public List<Stay> listByUser(String username) {
@@ -30,7 +38,14 @@ public class StayService {
         return stay;
     }
 
-    public void add(Stay stay) {
+    public void add(Stay stay, MultipartFile[] images) {
+        List<StayImage> stayImages = Arrays.stream(images)
+                .filter(image -> !image.isEmpty())
+                .parallel()
+                .map(imageStorageService::save)
+                .map(mediaLink -> new StayImage(mediaLink, stay))
+                .collect(Collectors.toList());
+        stay.setImages(stayImages);
         stayRepository.save(stay);
     }
 
