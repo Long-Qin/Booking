fcc65c45614c8d5448b3169450a74eb21e991b38
Long-Qin <mrmrmr.wayne@gmail.com>
2023-08-19 20:50:53 -0400

    Added stayImage and Google cloud service for image store

--- src/main/java/com/example/staybooking/model/Stay.java
+++ src/main/java/com/example/staybooking/model/Stay.java
@@ -1,17 +1,19 @@
 package com.example.staybooking.model;
 
-import com.example.staybooking.model.StayReservedDate;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+
 import javax.persistence.*;
 import java.util.List;
+
 
 @Entity
 @Table(name = "stay")
 @JsonDeserialize(builder = Stay.Builder.class)
 public class Stay {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -30,7 +32,13 @@ public class Stay {
     @OneToMany(mappedBy = "stay", cascade = CascadeType.ALL, fetch=FetchType.LAZY)
     private List<StayReservedDate> reservedDates;
 
+
+    @OneToMany(mappedBy = "stay", cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+    private List<StayImage> images;
+
+
     public Stay() {}
+
 
     private Stay(Builder builder) {
         this.id = builder.id;
@@ -40,96 +48,280 @@ public class Stay {
         this.guestNumber = builder.guestNumber;
         this.host = builder.host;
         this.reservedDates = builder.reservedDates;
+        this.images = builder.images;
     }
+
+
+    public List<StayImage> getImages() {
+        return images;
+    }
+
+
+    public Stay setImages(List<StayImage> images) {
+        this.images = images;
+        return this;
+    }
+
 
     public Long getId() {
         return id;
     }
 
+
     public String getName() {
         return name;
     }
+
 
     public String getDescription() {
         return description;
     }
 
+
     public String getAddress() {
         return address;
     }
+
 
     public int getGuestNumber() {
         return guestNumber;
     }
 
+
     public User getHost() {
         return host;
     }
+
 
     public List<StayReservedDate> getReservedDates() {
         return reservedDates;
     }
 
+
     public static class Builder {
+
 
         @JsonProperty("id")
         private Long id;
 
+
         @JsonProperty("name")
         private String name;
+
 
         @JsonProperty("description")
         private String description;
 
+
         @JsonProperty("address")
         private String address;
+
 
         @JsonProperty("guest_number")
         private int guestNumber;
 
+
         @JsonProperty("host")
         private User host;
 
+
         @JsonProperty("dates")
         private List<StayReservedDate> reservedDates;
+
+
+        @JsonProperty("images")
+        private List<StayImage> images;
+
 
         public Builder setId(Long id) {
             this.id = id;
             return this;
         }
 
+
         public Builder setName(String name) {
             this.name = name;
             return this;
         }
+
 
         public Builder setDescription(String description) {
             this.description = description;
             return this;
         }
 
+
         public Builder setAddress(String address) {
             this.address = address;
             return this;
         }
+
 
         public Builder setGuestNumber(int guestNumber) {
             this.guestNumber = guestNumber;
             return this;
         }
 
+
         public Builder setHost(User host) {
             this.host = host;
             return this;
         }
+
 
         public Builder setReservedDates(List<StayReservedDate> reservedDates) {
             this.reservedDates = reservedDates;
             return this;
         }
 
+
         public Stay build() {
             return new Stay(this);
         }
+
+
+        public Builder setImages(List<StayImage> images) {
+            this.images = images;
+            return this;
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
