4fa2e4262e9d97cb20192f36b26dc082de4b4b73
Long-Qin <mrmrmr.wayne@gmail.com>
2023-08-22 16:24:16 -0400

    Added two exceptions handle wrong address and wrong geo-coding

--- src/main/java/com/example/staybooking/controller/CustomExceptionHandler.java
+++ src/main/java/com/example/staybooking/controller/CustomExceptionHandler.java
@@ -10,28 +10,46 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+
     @ExceptionHandler(UserAlreadyExistException.class)
-    public final ResponseEntity<String> handleUserAlreadyExistExceptions(Exception ex, WebRequest request) {
+    public final ResponseEntity<String> handleUserAlreadyExistExceptions(Exception ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
+
     @ExceptionHandler(UserNotExistException.class)
-    public final ResponseEntity<String> handleUserNotExistException(Exception ex) {
+    public final ResponseEntity<String> handleUserNotExistExceptions(Exception ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
+
     @ExceptionHandler(StayNotExistException.class)
-    public final ResponseEntity<String> handleStayNotExistException(Exception ex) {
+    public final ResponseEntity<String> handleStayNotExistExceptions(Exception ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+
     @ExceptionHandler(GCSUploadException.class)
-    public final ResponseEntity<String> handleGCSUploadException(Exception ex, WebRequest request){
+    public final ResponseEntity<String> handleGCSUploadExceptions(Exception ex, WebRequest request) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+
     @ExceptionHandler(InvalidSearchDateException.class)
-    public final ResponseEntity<String> handleInvalidSearchDateException(Exception ex, WebRequest request){
+    public final ResponseEntity<String> handleInvalidSearchDateExceptions(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(GeoCodingException.class)
+    public final ResponseEntity<String> handleGeoCodingExceptions(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(InvalidStayAddressException.class)
+    public final ResponseEntity<String> handleInvalidStayAddressExceptions(Exception ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
+
